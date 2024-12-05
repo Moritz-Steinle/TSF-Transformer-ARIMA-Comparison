5b96f457466233adc6131130eecb630793f30425
Moritz Steinle <moritz.steinle@uni-ulm.de>
2024-12-05 11:53:43 +0100

    Finish /data overhaul

--- data/from_db.py
+++ data/from_db.py
@@ -1,18 +1,31 @@
 import os
 
 import influxdb_client
+from pandas import DataFrame
 
 from config import config
 
 
-def fetch(
+def fetch_from_influxdb(
     target_filename: str,
     start_date="2024-06-18T06:00:00Z",
     end_date="2024-09-24T00:00:00Z",
-):
-    org = "uulm"
+) -> DataFrame:
+    """
+    Fetches data from an InfluxDB database and writes it to a CSV file.
+    Args:
+        target_filename (str): The name of the CSV file to write the data to.
+        start_date (str, optional): The start date for the data query in ISO 8601 format.
+            Defaults to "2024-06-18T06:00:00Z".
+        end_date (str, optional): The end date for the data query in ISO 8601 format.
+            Defaults to "2024-09-24T00:00:00Z".
+    Returns:
+        DataFrame: A pandas DataFrame containing the fetched data.
+    Raises:
+        Exception: If there is an error in querying the InfluxDB or writing the data to the CSV file.
+    """
     client = influxdb_client.InfluxDBClient(
-        url=config.influxdb_url, token=config.influxdb_token, org=org
+        url=config.influxdb_url, token=config.influxdb_token, org=config.influxdb_org
     )
     query_api = client.query_api()
     query = f"""from(bucket: "plant_sensor_data")
@@ -25,14 +38,22 @@ def fetch(
         |> keep(columns: ["_time", "moisture"])
         |> yield(name: "mean")"""
 
-    dataframes = query_api.query_data_frame(org=org, query=query)
-    write_dataframe_to_csv(dataframes, target_filename=target_filename)
-    print(f"Fetched {len(dataframes)} rows to {target_filename}")
-    return dataframes
+    dataset = query_api.query_data_frame(org=config.influxdb_org, query=query)
+    write_dataframe_to_csv(dataset, target_filename=target_filename)
+    print(f"Fetched {len(dataset)} rows to {target_filename}")
+    return dataset
 
 
-def write_dataframe_to_csv(dataframes, target_filename: str):
+def write_dataframe_to_csv(dataset: DataFrame, target_filename: str) -> None:
+    """
+    Writes a pandas DataFrame to a CSV file to the data-files directory.
+    Parameters:
+        dataset (DataFrame): The DataFrame to be written to a CSV file.
+        target_filename (str): The name of the target CSV file (without the .csv extension).
+    Returns:
+        None
+    """
     target_filename = f"{target_filename}.csv"
     dirname = os.path.dirname(os.path.abspath(__file__))
-    filename_with_path = os.path.join(dirname, target_filename)
-    dataframes.to_csv(filename_with_path, index=False)
+    filename_with_path = os.path.join(dirname, "data-files", target_filename)
+    dataset.to_csv(filename_with_path, index=False)
